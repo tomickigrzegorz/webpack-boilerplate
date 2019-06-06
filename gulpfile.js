947fe654a64c7f4fcf0e6c23f7c051c326118c02
Grzegorz Tomicki <gtomicki@gmail.com>
2019-06-06 22:44:00 +0200

    removal gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const webp = require('gulp-webp');
-
-gulp.task('img', () =>
-  gulp
-    .src('./sources/images/**/*.jpg')
-    .pipe(webp({ quality: 60 }))
-    .pipe(gulp.dest('./sources/images/'))
-);
