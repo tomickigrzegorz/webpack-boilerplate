9ca08772257059c1c3250a3d049c1e33ae429866
Grzegorz Tomicki <gtomik@gmail.com>
2021-12-06 14:50:31 +0100

    small improvements

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -29,55 +29,55 @@ const oneFileCss = {
 };
 
 // configure Optimization
-const configureOptimization = () => {
-  return {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-    // ...oneFileCss,
-  };
+const configureOptimization = {
+  minimize: true,
+  minimizer: [new TerserPlugin()],
+  // ...oneFileCss,
 };
 
 // configure MiniCssExtract
-const configureMiniCssExtract = () => {
-  return {
+const configureMiniCssExtract = [
+  {
     filename: 'vendor/css/[name].[fullhash].css',
     chunkFilename: 'vendor/css/[name].[fullhash].css',
-  };
-};
+  },
+];
 
 // configure Service Worker
-const configureSW = () => {
-  return {
+const configureSW = [
+  {
     clientsClaim: true,
     skipWaiting: true,
     directoryIndex: 'index.html',
     offlineGoogleAnalytics: true,
-  };
-};
+
+    // we exclude photos
+    exclude: ['images'],
+  },
+];
 
 // configure Copy
-const configureCopy = () => {
-  return {
-    patterns: [
-      {
-        from: 'sources/assets/',
-        to: 'assets/',
+const configureCopy = {
+  patterns: [
+    {
+      from: 'sources/assets/',
+      to: 'assets/',
+    },
+    {
+      from: 'sources/images/',
+      to: 'images/',
+      // blocking file copying by plugin webpack will
+      // do it for you and rename it with a hash
+      globOptions: {
+        ignore: ['**.svg'],
       },
-      {
-        from: 'sources/images/',
-        to: 'images/',
-        // blocking file copying by plugin webpack will
-        // do it for you and rename it with a hash
-        globOptions: {
-          ignore: ['**.svg'],
-        },
-      },
-    ],
-  };
+    },
+  ],
 };
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  // line 18 in package.json
   target: 'browserslist',
   module: {
     rules: [
@@ -98,7 +98,6 @@ module.exports = merge(baseConfig, {
       },
     ],
   },
-  optimization: configureOptimization(),
   plugins: [
     // when we run the production build then
     // the docs folder is cleared
@@ -109,14 +108,15 @@ module.exports = merge(baseConfig, {
 
     // we extract scss files from js and create
     // separate files for individual pages
-    new MiniCssExtractPlugin(configureMiniCssExtract()),
+    new MiniCssExtractPlugin(...configureMiniCssExtract),
 
+    // https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
     // we create a service-worker for our data
-    new WorkboxPlugin.GenerateSW(configureSW()),
+    new WorkboxPlugin.GenerateSW(...configureSW),
 
     // we copy all necessary graphic files
     // and assets to build folder
-    new CopyWebpackPlugin(configureCopy()),
+    new CopyWebpackPlugin({ ...configureCopy }),
 
     // we create a global variable that
     // we use in pug and we can use in js
@@ -131,4 +131,11 @@ module.exports = merge(baseConfig, {
       openAnalyzer: true,
     }),
   ],
+  optimization: { ...configureOptimization },
+  performance: {
+    // https://webpack.js.org/configuration/performance/#performancehints
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 });
